b95962dac54b077e72fb0dfff5d83d4abc8eb469
Manny Sran <manny@modelliving.com>
2022-09-18 19:55:51 -0400

    Trying different port fix

--- conf/config.ts
+++ conf/config.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
 var config = JSON.parse('{}');
 
-config.httpPort = process.env.HTTP_PORT || 5000;
+config.httpPort = process.env.PORT || 5000;
 config.environment = process.env.NODE_ENV || 'dev';
 config.logLevel = process.env.LOG_LEVEL || 'debug';
 config.riotApiKey = process.env.RIOT_API_KEY || '';
